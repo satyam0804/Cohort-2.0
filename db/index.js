342160d5e8839652b696c15af1f7eb150e47ae83
Satyam Lakhera <75569926+satyam0804@users.noreply.github.com>
2024-03-31 19:53:08 +0530

    Updated the Schema of the DB

--- db/index.js
+++ db/index.js
@@ -1,27 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect("mongodb+srv://satyam08:H@ckme4fun@cluster0.k771ssh.mongodb");
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
+  // Schema definition here
+  username: String,
+  password: String,
 });
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
+  // Schema definition here
+  username: String,
+  password: String,
 });
 
 const CourseSchema = new mongoose.Schema({
-    // Schema definition here
+  // Schema definition here
+  title: String,
+  description: String,
+  price: Number,
 });
 
-const Admin = mongoose.model('Admin', AdminSchema);
-const User = mongoose.model('User', UserSchema);
-const Course = mongoose.model('Course', CourseSchema);
+const Admin = mongoose.model("Admin", AdminSchema);
+const User = mongoose.model("User", UserSchema);
+const Course = mongoose.model("Course", CourseSchema);
 
 module.exports = {
-    Admin,
-    User,
-    Course
-}
+  Admin,
+  User,
+  Course,
+};
